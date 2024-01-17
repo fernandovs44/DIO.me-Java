9ca16a3195c97e0187b6a9095c3241486067f7c3
“Fernando <fernandovs44@gmail.com>
2024-01-17 18:10:00 -0300

    update variables project

---
+++ Java-Introduction/src/Variables.java
@@ -0,0 +1,11 @@
+public class Variables {
+    public static void main(String[] args) throws Exception {
+
+        String name = "Fernando";
+        int age = 35;
+        float height = 1.71;
+        char gender = "M";
+        boolean alive = true;
+
+    }
+}
