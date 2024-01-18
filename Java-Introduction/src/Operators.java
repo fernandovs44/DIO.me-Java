735b46b8b2478198f905843fc51a4224caaf5479
“Fernando <fernandovs44@gmail.com>
2024-01-18 17:49:10 -0300

    Update operators project

--- Java-Introduction/src/Operators.java
+++ Java-Introduction/src/Operators.java
@@ -2,6 +2,22 @@ public class Operators {
 
   public static void main(String[] args) {
 
+    int addition = 10 + 10;
+    int subtraction = 10 - 10;
+    int multiplication = 10 * 10;
+    int division = 10 / 10;
+    int modulus = 10 % 10;
+    int increment = ++addition;
+    int decrement = --subtraction;
+
+    System.out.println(addition);
+    System.out.println(subtraction);
+    System.out.println(multiplication);
+    System.out.println(division);
+    System.out.println(modulus);
+    System.out.println(increment);
+    System.out.println(decrement);
+
   }
 
 }
