3d995a2315da795305472508fb9c98c7b870145b
“Fernando <fernandovs44@gmail.com>
2024-01-22 19:11:13 -0300

    Update Methods

---
+++ dio-java-introduction/src/java-introduction-course/MethodSmatTv.java
@@ -0,0 +1,5 @@
+public class MethodSmatTv {
+  boolean on = false;
+  int channel = 1;
+  int volume = 1;
+}
---
+++ dio-java-introduction/src/java-introduction-course/MethodUserSmartTv.java
@@ -0,0 +1,7 @@
+public class MethodUserSmartTv {
+  public static void main(String[] args) {
+    MethodSmatTv smatTv = new MethodSmatTv();
+    System.out.println(smatTv.on);
+  }
+
+}
