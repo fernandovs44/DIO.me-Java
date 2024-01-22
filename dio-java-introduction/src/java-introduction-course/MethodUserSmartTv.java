1c482bdf0c7bb416b604cccf32f78d879de13c4c
“Fernando <fernandovs44@gmail.com>
2024-01-22 19:27:13 -0300

    Update methods project

--- dio-java-introduction/src/java-introduction-course/MethodUserSmartTv.java
+++ dio-java-introduction/src/java-introduction-course/MethodUserSmartTv.java
@@ -1,7 +1,29 @@
 public class MethodUserSmartTv {
   public static void main(String[] args) {
-    MethodSmatTv smatTv = new MethodSmatTv();
-    System.out.println(smatTv.on);
+
+    MethodSmatTv methodsmatTv = new MethodSmatTv();
+
+    System.out.println("The Tv is: " + methodsmatTv.on);
+    System.out.println("The channel is: " + methodsmatTv.channel);
+    System.out.println("The volume is: " + methodsmatTv.volume);
+
+    methodsmatTv.turnOn();
+    System.out.println("The Tv is: " + methodsmatTv.on);
+
+    methodsmatTv.changeChannel();
+    System.out.println("The channel is: " + methodsmatTv.channel);
+
+    methodsmatTv.changeVolume();
+    System.out.println("The volume is: " + methodsmatTv.volume);
+
+    methodsmatTv.upVolume();
+    methodsmatTv.upVolume();
+    System.out.println("The volume is: " + methodsmatTv.volume);
+
+    methodsmatTv.downChannel();
+    methodsmatTv.downChannel();
+    System.out.println("The channel is: " + methodsmatTv.channel);
+
   }
 
 }
