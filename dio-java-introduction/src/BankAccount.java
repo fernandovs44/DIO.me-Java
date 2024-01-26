9d4a1d9e294d84acac2be57ed0c71ab92b37b120
“Fernando <fernandovs44@gmail.com>
2024-01-26 20:00:48 -0300

    Update BankAccount project

--- dio-java-introduction/src/BankAccount.java
+++ dio-java-introduction/src/BankAccount.java
@@ -1,6 +1,24 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class BankAccount {
 
   public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Type your account!");
+    int account = scanner.nextInt();
+    System.out.println("Type your bank agency!");
+    String agency = scanner.next();
+    System.out.println("Type your name!");
+    String name = scanner.next();
+    System.out.println("Type your balance!");
+    double balance = scanner.nextDouble();
+
+    System.out
+        .println("Hello " + name + ", thank you for creating an account with our bank, your bank agency is " + agency
+            + ", account number " + account + " and your balance $" + balance + " is now available for withdrawal.");
 
   }
 
