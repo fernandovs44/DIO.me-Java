04f6d0cd6a24e6e6ddbdda7d69685856025de878
“Fernando <fernandovs44@gmail.com>
2024-02-19 18:19:33 -0300

    Update TryCath project

--- dio-java-introduction/src/TryCath.java
+++ dio-java-introduction/src/TryCath.java
@@ -1,3 +1,27 @@
+import java.util.Scanner;
+import java.util.Locale;
+
 public class TryCath {
 
+  public static void main(String[] args) {
+
+    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+    System.out.println("Type your name");
+    String name = scanner.next();
+
+    System.out.println("Type your last name");
+    String LastName = scanner.next();
+
+    System.out.println("Type your age");
+    String age = scanner.next();
+
+    System.out.println("Type your height");
+    String height = scanner.next();
+
+    System.out
+        .println(
+            "My name is " + name + " " + LastName + ". I'm " + age + " years old and my height is " + height + ".");
+
+  }
 }
