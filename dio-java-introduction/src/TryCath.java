9de77eab8614a88bbf8384a60ae87283a2351b1a
“Fernando <fernandovs44@gmail.com>
2024-02-19 18:30:57 -0300

    Update TryCatch project

--- dio-java-introduction/src/TryCath.java
+++ dio-java-introduction/src/TryCath.java
@@ -1,27 +1,31 @@
 import java.util.Scanner;
+import java.util.InputMismatchException;
 import java.util.Locale;
 
 public class TryCath {
 
   public static void main(String[] args) {
+    try {
+      Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
-    Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+      System.out.println("Type your name");
+      String name = scanner.next();
 
-    System.out.println("Type your name");
-    String name = scanner.next();
+      System.out.println("Type your last name");
+      String LastName = scanner.next();
 
-    System.out.println("Type your last name");
-    String LastName = scanner.next();
+      System.out.println("Type your age");
+      Integer age = scanner.nextInt();
 
-    System.out.println("Type your age");
-    String age = scanner.next();
+      System.out.println("Type your height");
+      Double height = scanner.nextDouble();
 
-    System.out.println("Type your height");
-    String height = scanner.next();
+      System.out
+          .println(
+              "My name is " + name + " " + LastName + ". I'm " + age + " years old and my height is " + height + ".");
 
-    System.out
-        .println(
-            "My name is " + name + " " + LastName + ". I'm " + age + " years old and my height is " + height + ".");
-
+    } catch (InputMismatchException e) {
+      System.err.println("Age and Height fields must be numeric");
+    }
   }
 }
